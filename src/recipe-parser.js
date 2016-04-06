a216dfb8fa410583245b71fbe380ce59e2c24b67
Eduardo Lanchares <eduardo@elanchares.com>
2016-04-06 21:53:49 +0200

    Use markdown parser

--- src/recipe-parser.js
+++ src/recipe-parser.js
@@ -1,28 +1,84 @@
+import { markdown } from 'markdown';
+
 /**
  * [extractBlocks description]
  * @param  {String} text
- * @return {Array<String>}
+ * @return {Tree}
  */
 function extractBlocks(text) {
-  return text.split(/^#{1,2}\s?\w+$/m);
+  return markdown.parse(text);
 }
 
 /**
  * [parseIngredients description]
- * @param  {String}
+ * @param  {Tree} blocks
  * @return {Array<String>}
  */
-function parseIngredients(block) {
-  return block.split('\n').map((line) => line.split('* ')[1]);
+function parseIngredients(blocks) {
+  const headerPositions = blocks.reduce((acum, node, index) => {
+    if (!Array.isArray(node)) return acum;
+
+    if (isHeaderBlock(node, 2)) {
+      acum.push(index);
+    }
+
+    return acum;
+  }, []);
+
+  const firstHeaderBounds = headerPositions.slice(0, 2);
+  const firstHeaderInnerBlocks = blocks.slice(firstHeaderBounds[0] + 1, firstHeaderBounds[1]);
+
+  const flatIngredientsList = firstHeaderInnerBlocks.reduce((acum, node) => {
+    if (!Array.isArray(node)) return acum;
+
+    if (isListBlock(node)) {
+      return acum.concat(node.slice(1));
+    }
+
+    return acum;
+  }, []);
+
+  return flatIngredientsList.map((node) => node[1]);
 }
 
 /**
- * [compactLines description]
- * @param  {Array<String>} lines
- * @return {Array<String>}
+ * [isEmptyNode description]
+ * @param  {Array}  node
+ * @return {Boolean}
  */
-function compactLines(lines) {
-  return lines.filter((line) => !!line);
+function isEmptyNode(node) {
+  return node.length === 0;
+}
+
+/**
+ * [isListItemBlock description]
+ * @param  {Array}  node
+ * @return {Boolean}
+ */
+function isListItemBlock(node) {
+  return !isEmptyNode(node) &&
+         node[0] === 'listitem';
+}
+
+/**
+ * [isListBlock description]
+ * @param  {Array}  node
+ * @return {Boolean}
+ */
+function isListBlock(node) {
+  return !isEmptyNode(node) &&
+         node[0] === 'bulletlist';
+}
+
+/**
+ * [isHeaderBlock description]
+ * @param  {Array}  node
+ * @return {Boolean}
+ */
+function isHeaderBlock(node, level = 1) {
+  return !isEmptyNode(node) &&
+         node[0] === 'header' &&
+         node[1] && node[1].level === level;
 }
 
 /**
@@ -31,10 +87,11 @@ function compactLines(lines) {
  * @return {Array<String>}
  */
 export function extractIngredients(text) {
-  // Skip first line (empty line)
-  const blocks = extractBlocks(text).slice(1);
+  const blocks = extractBlocks(text);
 
   if (!blocks.length) return [];
 
-  return compactLines(parseIngredients(blocks[0]));
+  const ingredients = parseIngredients(blocks);
+
+  return ingredients;
 }
